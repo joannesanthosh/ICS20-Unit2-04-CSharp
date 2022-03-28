a865eeb1d3f19db475e85ef7809553f8ef6fa27c
joannesanthosh <99355156+joannesanthosh@users.noreply.github.com>
2022-03-28 23:12:51 +0000

    fix main.cs

--- main.cs
+++ main.cs
@@ -7,12 +7,12 @@ using System;
 
 class Program 
 {
-    public static void Main (string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine ("This program finds the area of a triangle");
-        Console.WriteLine ("Enter the base (mm): 5");
-        Console.WriteLine ("Enter the height (mm): 10");
-        Console.WriteLine ("The area of a triangle is: 25mm²");
+        Console.WriteLine("This program finds the area of a triangle");
+        Console.WriteLine("Enter the base (mm): 5");
+        Console.WriteLine("Enter the height (mm): 10");
+        Console.WriteLine("The area of a triangle is: 25mm²");
         Console.WriteLine("\nDone.");
     }
 }
