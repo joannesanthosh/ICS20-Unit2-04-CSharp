1fa4b713f1b5210e521a2678727099a80a6b9d15
joannesanthosh <99355156+joannesanthosh@users.noreply.github.com>
2022-03-28 23:06:37 +0000

    fix main.cs

--- main.cs
+++ main.cs
@@ -1,12 +1,18 @@
+// Created by: Joanne Santhosh
+// Created on: Mar 2022
+//
+// This program displays, "The Area of a Triangle, with JS"
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("This program finds the area of a triangle");
-    Console.WriteLine ("Enter the base (mm): 5");
-    Console.WriteLine ("Enter the height (mm): 10");
-   
-    Console.WriteLine ("The area of a triangle is: 25mm²");
-    Console.WriteLine("\nDone.");
-  }
+class Program 
+{
+    public static void Main (string[] args)
+    {
+        Console.WriteLine ("This program finds the area of a triangle");
+        Console.WriteLine ("Enter the base (mm): 5");
+        Console.WriteLine ("Enter the height (mm): 10");
+        Console.WriteLine ("The area of a triangle is: 25mm²");
+        Console.WriteLine("\nDone.");
+    }
 }
