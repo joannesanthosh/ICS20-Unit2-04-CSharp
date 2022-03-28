40738f4b033c072463957249060a2d55ce3f59eb
joannesanthosh <99355156+joannesanthosh@users.noreply.github.com>
2022-03-28 22:53:56 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("This program finds the area of a triangle");
+    Console.WriteLine ("Enter the base (mm): 5");
+    Console.WriteLine ("Enter the height (mm): 10");
+   
+    Console.WriteLine ("The area of a triangle is: 25mm²");
+    Console.WriteLine("\nDone.");
+  }
+}
